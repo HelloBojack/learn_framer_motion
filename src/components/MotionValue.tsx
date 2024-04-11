a7fc0337970527b21b823664e777fcfc72602aa2
HelloBojack <xlk970115@gmail.com>
2024-04-11 15:47:42 +0800

    feat: useSpring

--- src/components/MotionValue.tsx
+++ src/components/MotionValue.tsx
@@ -1,21 +1,22 @@
-import { motion, useMotionValue } from "framer-motion";
+import { motion, useSpring } from "framer-motion";
 
 const MotionValueComponent = () => {
-  const scale = useMotionValue(1);
+  const scale  = useSpring(1);
   
   return (
     <div className="flex flex-col items-center">
       <motion.div
-        className="w-[100px] h-[100px] bg-red-500 rounded-[25px]"
+        className="w-[100px] h-[100px] bg-red-500 rounded-[50px]"
         style={{ scale }}
-        />
+      />
       <input
         type="range"
         className="mt-[50px] z-10"
         min={0.5}
         max={5}
-        step={0.1}
-        onChange={e => scale.set(e.target.value)}
+        step={0.01}
+        defaultValue={1}
+        onChange={e => scale.set(parseFloat(e.target.value))}
       />
     </div>
   )
